af32d25660c0a770f42db414f95d31d84fbc2760
Manoel-iranilson <manoelvieirafilho0@gmail.com>
2022-11-02 23:31:41 -0300

    tela do chat

---
+++ src/pages/chat/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Sidebar from '../../components/Sidebar';
+
+// import { Container } from './styles';
+
+function Chat() {
+    return (
+        <Sidebar />
+    );
+}
+
+export default Chat;
