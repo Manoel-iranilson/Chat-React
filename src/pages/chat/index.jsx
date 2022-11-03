8036551a4ddecf989263f6bc0ddab4b41540759a
Manoel-iranilson <manoelvieirafilho0@gmail.com>
2022-11-03 12:15:35 -0300

    configurando parte do chat

--- src/pages/chat/index.jsx
+++ src/pages/chat/index.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import { Flex, Text } from '@chakra-ui/react';
+import React, { useState } from 'react';
 import Sidebar from '../../components/Sidebar';
 
 // import { Container } from './styles';
 
 function Chat() {
+    const [userChat, setUserChat] = useState(null)
+
+    console.log(userChat);
     return (
-        <Sidebar />
+        <Flex>
+            <Sidebar userChat={userChat} setUserChat={setUserChat} />
+            <Text>{userChat?.name}</Text>
+
+        </Flex>
     );
 }
 
