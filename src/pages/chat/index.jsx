334626bf713015eb2ab31a98a29ecb30ad5fafa0
Manoel-iranilson <manoelvieirafilho0@gmail.com>
2022-11-03 23:16:11 -0300

    pagina do chat

--- src/pages/chat/index.jsx
+++ src/pages/chat/index.jsx
@@ -1,19 +1,55 @@
-import { Flex, Text } from '@chakra-ui/react';
 import React, { useState } from 'react';
+import { Flex, Text, Input, Button } from '@chakra-ui/react';
+import { BsPersonCircle } from "react-icons/bs"
 import Sidebar from '../../components/Sidebar';
+import MyContext from '../../context/myContext';
+import ChatBody from '../../components/ChatBody';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth, db } from '../../services/firebase';
+import firebase from 'firebase/compat/app';
 
-// import { Container } from './styles';
 
 function Chat() {
     const [userChat, setUserChat] = useState(null)
+    const [user] = useAuthState(auth);
+    const [message, setMessage] = useState("");
 
-    console.log(userChat);
+    console.log(userChat?.chatId);
+
+    const sendMessage = (e) => {
+        e.preventDefault();
+
+        db.collection("chats").doc(userChat?.chatId).collection("messeges").add({
+            message: message,
+            user: user.email,
+            photoURL: user.photoURL,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+
+        });
+
+        setMessage("")
+
+    }
+
     return (
-        <Flex>
-            <Sidebar userChat={userChat} setUserChat={setUserChat} />
-            <Text>{userChat?.name}</Text>
+        <MyContext.Provider value={{ userChat, setUserChat }}>
+            <Flex>
+                <Sidebar />
+                <Flex flexDirection={"column"}>
+                    <Flex bg={"#1f2c34"} p={5} pt={8} >
+                        {userChat?.photoURL ? <Image src={userChat?.photoURL} /> : <BsPersonCircle size={40} color={"#fff"} />}
+                        <Text textColor={"#fff"} fontSize={20} pl={5}>{userChat?.name}</Text>
+                    </Flex>
+                    <ChatBody chatId={userChat?.chatId} />
+                    <Flex >
+                        <Input bg={"#1f2c34"} w={"30rem"} color={"#fff"} placeholder='Basic usage' value={message} onChange={(e) => setMessage(e.target.value)} />
+                        <Button onClick={sendMessage}>enviar</Button>
+                    </Flex>
+                </Flex>
+            </Flex>
 
-        </Flex>
+
+        </MyContext.Provider >
     );
 }
 
