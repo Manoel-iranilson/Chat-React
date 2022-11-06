a7c390990186519966f2ad37bf212855d6727774
Manoel-iranilson <manoelvieirafilho0@gmail.com>
2022-11-06 13:40:20 -0300

    criação do componente message

--- src/components/ChatBody/index.jsx
+++ src/components/ChatBody/index.jsx
@@ -1,15 +1,12 @@
-import { Flex } from '@chakra-ui/react';
+import { Flex, Text, Box } from '@chakra-ui/react';
 import React, { useEffect, useRef } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
-
 import { useCollection } from 'react-firebase-hooks/firestore';
 import { auth, db } from '../../services/firebase';
-
-
+import image from '../../assets/image.webp'
 
 function ChatBody({ chatId }) {
     const [userLoggenIn] = useAuthState(auth)
-
 
     const [messagesRes] = useCollection(
         db
@@ -28,16 +25,23 @@ function ChatBody({ chatId }) {
     }, [messagesRes]);
 
     return (
-        <Flex h={28} bg={"#000"} color={"#fff"} >
-
-            {messagesRes?.docs.map((message) => (
-
-                <h3>{message.data().message} </h3>
-
-
-            ))
+        <Flex h={470} direction='column' w={1030} overflowY={"auto"} backgroundImage={image} color={"#fff"} >
+            {messagesRes?.docs.map((message) => {
+                if (userLoggenIn.email === message.data().user) {
+                    return (
+                        <Box textAlign={'end'} borderRadius={5} bg={"#8FBC8F"} ml={"50rem"} mt={5} mb={4} mr={5} >
+                            <Text mr={5} fontSize={20} >{message.data().message}</Text>
+                        </Box>
+                    )
+                } else {
+                    return (
+                        <Box borderRadius={5} bg={"	#808080"} w={"12rem"} m={5} >
+                            <Text ml={5} fontSize={20} >{message.data().message}</Text>
+                        </Box>)
+                }
             }
-
+            )
+            }
 
         </Flex>
     );
