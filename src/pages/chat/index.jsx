8a71dc50c4ef898119a95a74981e53c99671f6e3
Manoel-iranilson <manoelvieirafilho0@gmail.com>
2022-11-07 19:30:34 -0300

    responsividade e corrigir layout

--- src/pages/chat/index.jsx
+++ src/pages/chat/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Flex, Text, Input, Button, Image, Center } from '@chakra-ui/react';
+import { Flex, Text, Input, Button, Image, Center, useBreakpointValue } from '@chakra-ui/react';
 import { BsPersonCircle } from "react-icons/bs"
 import { MdSend } from "react-icons/md"
 import Sidebar from '../../components/Sidebar';
@@ -11,11 +11,10 @@ import firebase from 'firebase/compat/app';
 import image from "../../assets/image.webp"
 
 function Chat() {
+    const isDesktop = useBreakpointValue({ lg: "none" });
     const [userChat, setUserChat] = useState(null)
     const [user] = useAuthState(auth);
     const [message, setMessage] = useState("");
-
-    console.log(userChat);
 
     const sendMessage = (e) => {
         e.preventDefault();
@@ -34,32 +33,54 @@ function Chat() {
 
     return (
         <MyContext.Provider value={{ userChat, setUserChat }}>
-            <Flex>
-                <Sidebar />
+            <Flex h="100vh" w="100%" >
+                {isDesktop ?
+                    <>
+                        <Sidebar />
+                        {userChat == null ?
+                            <Flex w={"80vw"} h={"100vh"} backgroundImage={image} >
+                            </Flex>
+                            :
+                            <Flex w="100%" h="100vh" flexDirection={"column"}>
+                                <Flex h={105} bg={"#1f2c34"} p={3} >
+                                    <Center>
+                                        {userChat.photoURL ? <Image boxSize={"20"} borderRadius={100} src={userChat?.photoURL} /> : <BsPersonCircle size={40} color={"#fff"} />}
+                                        <Text textColor={"#fff"} fontSize={20} pl={5}>{userChat?.name}</Text>
+                                    </Center>
+                                </Flex>
+                                <ChatBody chatId={userChat?.chatId} />
+                                <Flex bg={"#1f2c34"}>
+                                    <Input bg={"#1f2c34"} color={"#fff"} placeholder='Escreva aqui...' value={message} onChange={(e) => setMessage(e.target.value)} />
+                                    <Button onClick={sendMessage}> <MdSend size={20} /> </Button>
+                                </Flex>
+                            </Flex>
+                        }
 
-                {userChat == null ?
-                    <Flex w={"100vw"} h={"100vh"} backgroundImage={image} >
-                    </Flex>
+                    </>
                     :
+                    <>
+                        {userChat == null ?
+                            <Flex w={"100vw"} h={"100vh"} backgroundImage={image} >
+                                <Sidebar />
+                            </Flex>
+                            :
+                            <Flex flexDirection={"column"} bg="#1f2c34" w="100%" h="100%"  >
+                                <Center p={3} >
+                                    <Sidebar />
+                                    {userChat.photoURL ? <Image boxSize={"16"} borderRadius={100} src={userChat?.photoURL} /> : <BsPersonCircle size={63} color={"#fff"} />}
+                                    <Text textColor={"#fff"} fontSize={20} pl={5}>{userChat?.name}</Text>
+                                </Center>
+                                <ChatBody chatId={userChat?.chatId} />
 
-                    <Flex flexDirection={"column"}>
-                        <Flex h={105} bg={"#1f2c34"} p={3} >
-                            <Center>
-                                {userChat?.photoURL ? <Image boxSize={"20"} borderRadius={100} src={userChat?.photoURL} /> : <BsPersonCircle size={40} color={"#fff"} />}
-                                <Text textColor={"#fff"} fontSize={20} pl={5}>{userChat?.name}</Text>
-                            </Center>
-                        </Flex>
-                        <ChatBody chatId={userChat?.chatId} />
-                        <Flex bg={"#1f2c34"}>
-                            <Input bg={"#1f2c34"} w={"60rem"} color={"#fff"} placeholder='Escreva aqui...' value={message} onChange={(e) => setMessage(e.target.value)} />
-                            <Button onClick={sendMessage}> <MdSend size={20} /> </Button>
-                        </Flex>
-                    </Flex>
-
+                                <Flex bg={"#1f2c34"}>
+                                    <Input bg={"#1f2c34"} color={"#fff"} placeholder='Escreva aqui...' value={message} onChange={(e) => setMessage(e.target.value)} />
+                                    <Button onClick={sendMessage}> <MdSend size={20} /> </Button>
+                                </Flex>
+                            </Flex>
+                        }
+                    </>
                 }
             </Flex>
-
-
         </MyContext.Provider >
     );
 }
