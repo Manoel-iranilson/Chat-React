9f5dbbeaf9c3345feca1b328ba5e2afe62c4d0a4
Manoel-iranilson <manoelvieirafilho0@gmail.com>
2022-11-09 12:24:45 -0300

    corrigindo photo

--- src/pages/chat/index.jsx
+++ src/pages/chat/index.jsx
@@ -45,7 +45,7 @@ function Chat() {
                             <Flex w="100%" h="100%" flexDirection={"column"}>
                                 <Flex h={105} bg={"#1f2c34"} p={3} >
                                     <Center>
-                                        {userChat.photoURL ? <Image boxSize={"20"} borderRadius={100} src={userChat?.photoURL} /> : <BsPersonCircle size={40} color={"#fff"} />}
+                                        {userChat.photoURL ? <Image boxSize={"20"} borderRadius={100} src={userChat.photoURL} /> : <BsPersonCircle size={40} color={"#fff"} />}
                                         <Text textColor={"#fff"} fontSize={20} pl={5}>{userChat?.name}</Text>
                                     </Center>
                                 </Flex>
