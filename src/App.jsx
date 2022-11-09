773535a5b125d2b60ab08dc04b240775058c7880
Manoel-iranilson <manoelvieirafilho0@gmail.com>
2022-11-09 15:05:21 -0300

    corrigindo importação

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { auth, db } from './services/firebase'
 import { useAuthState } from "react-firebase-hooks/auth"
 import PublicRoutes from './routes/public.routes';
 import PrivateRoutes from './routes/private.routes';
-import { useState } from 'react';
+
 
 function App() {
   const [user, setUser] = useAuthState(auth);
